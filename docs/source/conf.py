835a2a0bc3116d959b2f878b17e3af1facd62d34
Aslak Bergersen <aslak.bergersen@gmail.com>
2018-10-10 10:45:43 +0200

    Update conf.py in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,8 +57,8 @@ master_doc = 'morphMan_docs'
 
 # General information about the project.
 project = u'morphMan'
-copyright = u'2018, Aslak Bergersen & Henrik Kjeldsberg'
-author = u'Aslak Bergersen & Henrik Kjeldsberg'
+copyright = u'2018, Aslak W. Bergersen & Henrik A. Kjeldsberg'
+author = u'Aslak W. Bergersen & Henrik A. Kjeldsberg'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -233,7 +233,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'morphMan_docs.tex', u'morphMan\\_docs Documentation',
-     u'Henrik Kjeldsberg', 'manual'),
+     u'Henrik A. Kjeldsberg & Aslak W. Bergersen', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
