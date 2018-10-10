e3f346e6b52db1bd1db5e8088eaa4c8dc6b195e0
Aslak Bergersen <aslak.bergersen@gmail.com>
2018-10-10 20:53:40 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+#templates_path = ['templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -148,7 +148,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+#html_static_path = []
 #['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
@@ -278,7 +278,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'morphMan_docs', u'morphMan_docs Documentation',
+    (master_doc, 'morphMan_docs', u'morphMan Documentation',
      author, 'morphMan_docs', 'One line description of project.',
      'Miscellaneous'),
 ]
