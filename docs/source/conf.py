4eca4ad42cab70906090f9959301421d402830fd
Aslak Bergersen <aslak.bergersen@gmail.com>
2018-10-11 12:52:37 +0200

    Change documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'morphMan_docs'
+#master_doc = 'morphMan_docs'
 
 # General information about the project.
 project = u'morphMan'
