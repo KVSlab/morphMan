23ec978a2bc54a791c98723f7595218a27c1aa43
hkjeldsberg <henrik.kjeldsberg@live.no>
2019-04-16 18:08:05 +0200

    Added automatic method of selecting branch to manipulate & where to move point

--- morphman/manipulate_branch.py
+++ morphman/manipulate_branch.py
@@ -5,6 +5,7 @@
 ##      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
 ##      PURPOSE.  See the above copyright notices for more information.
 
+import functools
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 from scipy.signal import argrelextrema
@@ -13,40 +14,8 @@ from scipy.signal import argrelextrema
 from common import *
 
 
-def get_longest_centerline(centerlines_complete):
-    longest = extract_single_line(centerlines_complete, 0)
-    for i in range(1, centerlines_complete.GetNumberOfLines()):
-        current_line = extract_single_line(centerlines_complete, i)
-        longest_length = len(get_curvilinear_coordinate(longest))
-        current_length = len(get_curvilinear_coordinate(current_line))
-        if current_length > longest_length:
-            longest = current_line
-
-    return longest
-
-
-def get_branch(branch_to_manipulate_number, centerlines_complete, longest_centerline):
-    break_points = dict()
-    for i in range(centerlines_complete.GetNumberOfLines()):
-        current_line = extract_single_line(centerlines_complete, i)
-        tolerance = get_tolerance(current_line)
-        for j in range(current_line.GetNumberOfPoints()):
-            p1 = np.asarray(current_line.GetPoint(j))
-            p2 = np.asarray(longest_centerline.GetPoint(j))
-            if distance(p1, p2) > tolerance:
-                break_points[j] = i
-                break
-
-    break_points_keys = sorted(break_points.keys())
-    print(break_points_keys)
-    selected_branch_id = break_points[break_points_keys[branch_to_manipulate_number]]
-    selected_branch = extract_single_line(centerlines_complete, selected_branch_id)
-
-    return selected_branch
-
-
-def manipulate_branch(input_filepath, output_filepath, smooth, smooth_factor, region_of_interest, region_points,
-                      poly_ball_size, no_smooth, no_smooth_point, resampling_step, angle):
+def manipulate_branch(input_filepath, output_filepath, smooth, smooth_factor, poly_ball_size, no_smooth,
+                      no_smooth_point, resampling_step, angle, branch_to_manipulate_number, branch_location):
     """
     Primary script for moving a selected branch of any blood vessel.
     Relies on a surface geometry of a 3D blood vessel network.
@@ -66,13 +35,13 @@ def manipulate_branch(input_filepath, output_filepath, smooth, smooth_factor, re
         output_filepath (str): Path to output the manipulated surface.
         smooth (bool): Smooth the Voronoi diagram.
         smooth_factor (float): Smoothing factor used for Voronoi diagram smoothing.
-        region_of_interest (str): Method for setting the region of interest ['manual' | 'commandline' | 'landmarking']
-        region_points (list): If region_of_interest is 'commandline', this a flatten list of the start and endpoint
         poly_ball_size (list): Resolution of polyballs used to create surface.
         no_smooth (bool): True of part of the model is not to be smoothed.
         no_smooth_point (ndarray): Point which is untouched by smoothing.
         resampling_step (float): Resampling length when resampling centerline.
         angle (float): Angle to rotate around new surface normal vector. Default i no rotation.
+        branch_to_manipulate_number (int): Number of branch to manipulate, ordered from up- to down-stream.
+        branch_location (ndarray): Point where branch to manipulate will be moved. Closest point on surface is selected.
     """
 
     # Input and output filenames
@@ -90,58 +59,40 @@ def manipulate_branch(input_filepath, output_filepath, smooth, smooth_factor, re
     centerlines_complete, voronoi, pole_ids = compute_centerlines(inlet, outlets, centerlines_path,
                                                                   capped_surface, resampling=resampling_step,
                                                                   smooth=False, base_path=base_path)
+
     if smooth:
         print("-- Smoothing Voronoi diagram")
         voronoi = prepare_voronoi_diagram(capped_surface, centerlines_complete, base_path,
                                           smooth, smooth_factor, no_smooth,
                                           no_smooth_point, voronoi, pole_ids)
 
-    # Define longest centerline as "main branch"
-    # If branch number is not none: sort centerlines -> select line n -> get branch
-    # Else -> click on branch -> get-diverging branch closest to point
-    # Then click / select place to put
-    branch_to_manipulate_number = 1
-    longest_centerline = get_longest_centerline(centerlines_complete)
-    branch_to_manipulate = get_branch(branch_to_manipulate_number, centerlines_complete, longest_centerline)
-    write_polydata(longest_centerline, "longest.vtp")
-
-    if region_points is None:
-        # Get region of interest
-        _, _, _, region_points, _ = get_line_to_change(capped_surface, centerlines_complete,
-                                                       region_of_interest, "bend", region_points, 0.0)
-        region_points = [[region_points[3 * i], region_points[3 * i + 1], region_points[3 * i + 2]]
-                         for i in range(len(region_points) // 3)]
-
-        # Get new position of branch on model surface
-        new_branch_pos_id, new_branch_pos = get_new_branch_position(capped_surface)
-
-    # Get branches from vmtk-branch-extractor
-    centerlines_branched = vmtk_branch_extractor(centerlines_complete)
-
-    # Set and get clipping points, centerlines and diverging centerlines
-    centerlines, diverging_centerlines, region_points, region_points_vtk, diverging_ids = \
-        find_region_of_interest_and_diverging_centerlines(centerlines_complete, region_points)
-    diverging_centerline_ispresent = diverging_centerlines is not None
-    diverging_id = None if len(diverging_ids) == 0 else diverging_ids[0]
-
-    # Handle diverging centerlines within region of interest
-    if diverging_centerline_ispresent:
-        diverging_centerline_end = get_diverging_centerline_end(diverging_centerlines, diverging_id, region_points)
+    # Select branch to manipulate
+    if branch_to_manipulate_number > 0:
+        longest_centerline = get_sorted_lines(centerlines_complete)[-1]
+        branch_to_manipulate = get_branch(branch_to_manipulate_number, centerlines_complete, longest_centerline)
     else:
-        raise RuntimeError("No diverging branch detected! Cannot translate nothing.")
-    diverging_centerline_end = branch_to_manipulate.GetPoint(branch_to_manipulate.GetNumberOfPoints() - 1)
+
+        shortest_centerlines = get_sorted_lines(centerlines_complete)[:-1]
+        branch_to_manipulate = pick_branch(capped_surface, shortest_centerlines)
+
+    # Get new position of branch on model surface
+    if branch_location is None:
+        new_branch_pos_id, new_branch_pos = pick_new_branch_position(capped_surface)
+    else:
+        new_branch_pos_id, new_branch_pos = get_new_branch_position(branch_location, capped_surface)
+
+    branch_to_manipulate_end_point = get_end_point(branch_to_manipulate)
+    centerlines = update_centerlines(centerlines_complete, branch_to_manipulate_end_point)
 
     # Select diverging branch from Brancher and compare with diverging branch found manually
-    diverging_centerline_branch = get_diverging_centerline_branch(centerlines_branched, diverging_centerline_end)
-
-    # Append remaining diverging centerlines back to centerliens
-    if diverging_centerlines.GetNumberOfLines() > 1:
-        centerlines = update_centerlines(centerlines, diverging_centerlines, diverging_centerline_end)
+    centerlines_branched = vmtk_branch_extractor(centerlines_complete)
+    diverging_centerline_branch = get_diverging_centerline_branch(centerlines_branched, branch_to_manipulate_end_point)
 
     # Clip Voronoi diagram into bend and remaining part of geometry
     print("-- Clipping Voronoi diagrams")
     voronoi_branch, voronoi_remaining = split_voronoi_with_centerlines(voronoi, [diverging_centerline_branch,
                                                                                  centerlines])
+
     # Get surface normals
     old_normal = get_estimated_surface_normal(diverging_centerline_branch)
     new_normal = get_exact_surface_normal(capped_surface, new_branch_pos_id)
@@ -164,9 +115,6 @@ def manipulate_branch(input_filepath, output_filepath, smooth, smooth_factor, re
     new_voronoi = merge_data([voronoi_remaining, moved_and_rotated_voronoi_branch])
     write_polydata(new_voronoi, new_voronoi_path)
 
-    write_polydata(centerlines, "cl_.vtp")
-    write_polydata(moved_and_rotated_centerline_branch, "moved_cl_.vtp")
-
     new_centerlines = merge_data([centerlines, moved_and_rotated_centerline_branch])
     write_polydata(new_centerlines, new_centerlines_path)
 
@@ -176,14 +124,22 @@ def manipulate_branch(input_filepath, output_filepath, smooth, smooth_factor, re
     print("-- Smoothing, clean, and check surface")
     new_surface = prepare_surface_output(new_surface, surface,
                                          new_centerlines, output_filepath,
-                                         test_merge=True, changed=True,
+                                         test_merge=False, changed=True,
                                          old_centerline=centerlines_complete)
 
     write_polydata(new_surface, output_filepath)
 
 
-def get_new_branch_position(capped_surface):
-    print("\nPlease select point to move branch in the render window.")
+def get_new_branch_position(branch_location, capped_surface):
+    surface_locator = get_locator(capped_surface)
+    new_branch_pos_id = surface_locator.FindClosestPoint(branch_location)
+    new_branch_pos = capped_surface.GetPoint(new_branch_pos_id)
+
+    return new_branch_pos_id, new_branch_pos
+
+
+def pick_new_branch_position(capped_surface):
+    print("\nPlease select new point to move branch in the render window.")
     seed_selector = vmtkPickPointSeedSelector()
     seed_selector.SetSurface(capped_surface)
     seed_selector.text = "Press space to select the point where you want to move the branch to, 'u' to undo.\n"
@@ -199,9 +155,15 @@ def get_diverging_centerline_end(diverging_centerlines, diverging_id, region_poi
     patch_diverging_line = clip_diverging_line(extract_single_line(diverging_centerlines, 0),
                                                region_points[0], diverging_id)
     diverging_centerline = extract_single_line(patch_diverging_line, 1)
-    diverging_centerline_end = diverging_centerline.GetPoint(diverging_centerline.GetNumberOfPoints() - 1)
+    diverging_centerline_end = get_end_point(diverging_centerline)
 
     return diverging_centerline_end
+
+
+def get_end_point(centerline):
+    centerline_end_point = centerline.GetPoint(centerline.GetNumberOfPoints() - 1)
+
+    return centerline_end_point
 
 
 def get_translation_parameters(centerlines, diverging_centerline_branch, new_branch_pos):
@@ -227,15 +189,15 @@ def get_exact_surface_normal(capped_surface, new_branch_pos_id):
 def get_diverging_centerline_branch(centerlines_branched, diverging_centerline_end):
     for i in range(centerlines_branched.GetNumberOfLines()):
         centerline_branch = extract_single_line(centerlines_branched, i)
-        if centerline_branch.GetPoint(centerline_branch.GetNumberOfPoints() - 1) == diverging_centerline_end:
+        if get_end_point(centerline_branch) == diverging_centerline_end:
             return centerline_branch
 
 
-def update_centerlines(centerlines, diverging_centerlines, diverging_centerline_end):
-    remaining_centerlines = [centerlines]
-    for i in range(diverging_centerlines.GetNumberOfLines()):
-        diverging_centerline = extract_single_line(diverging_centerlines, i)
-        if diverging_centerline.GetPoint(diverging_centerline.GetNumberOfPoints() - 1) != diverging_centerline_end:
+def update_centerlines(centerlines, diverging_centerline_end):
+    remaining_centerlines = []
+    for i in range(centerlines.GetNumberOfLines()):
+        diverging_centerline = extract_single_line(centerlines, i)
+        if get_end_point(diverging_centerline) != diverging_centerline_end:
             remaining_centerlines.append(diverging_centerline)
 
     centerlines = merge_data(remaining_centerlines)
@@ -345,6 +307,65 @@ def move_and_rotate_voronoi_branch(voronoi, dx, R_u, R_z, origo):
     return new_voronoi
 
 
+def get_sorted_lines(centerlines_complete):
+    lines = [extract_single_line(centerlines_complete, i) for i in range(centerlines_complete.GetNumberOfLines())]
+    sorted_lines = sorted(lines, key=functools.cmp_to_key(compare_to))
+
+    return sorted_lines
+
+
+def get_branch(branch_to_manipulate_number, centerlines_complete, longest_centerline):
+    break_points = dict()
+    for i in range(centerlines_complete.GetNumberOfLines()):
+        current_line = extract_single_line(centerlines_complete, i)
+        tolerance = get_tolerance(current_line)
+        for j in range(current_line.GetNumberOfPoints()):
+            p1 = np.asarray(current_line.GetPoint(j))
+            p2 = np.asarray(longest_centerline.GetPoint(j))
+            if distance(p1, p2) > tolerance:
+                break_points[j] = i
+                break
+
+    break_points_keys = sorted(break_points.keys())
+    selected_branch_id = break_points[break_points_keys[branch_to_manipulate_number - 1]]
+    selected_branch = extract_single_line(centerlines_complete, selected_branch_id)
+
+    return selected_branch
+
+
+def compare_to(line0, line1):
+    len0 = len(get_curvilinear_coordinate(line0))
+    len1 = len(get_curvilinear_coordinate(line1))
+    if len0 > len1:
+        return 1
+    return -1
+
+
+def pick_branch(capped_surface, centerlines):
+    print("\nPlease select the branch you with to move in the render window.")
+    # Select point on surface
+    seed_selector = vmtkPickPointSeedSelector()
+    seed_selector.SetSurface(capped_surface)
+    seed_selector.text = "Press space to select the branch you want to move, 'u' to undo.\n"
+    seed_selector.Execute()
+    branch_surface_point_id = seed_selector.GetTargetSeedIds().GetId(0)
+    surface_point = capped_surface.GetPoint(branch_surface_point_id)
+
+    # Find closest centerline
+    closest_dist = 1e9
+    branch_to_manipulate = None
+    for line_to_compare in centerlines:
+        locator = get_locator(line_to_compare)
+        closest_point_id = locator.FindClosestPoint(surface_point)
+        closest_point = line_to_compare.GetPoint(closest_point_id)
+        dist = distance(closest_point, surface_point)
+        if dist < closest_dist:
+            closest_dist = dist
+            branch_to_manipulate = line_to_compare
+
+    return branch_to_manipulate
+
+
 def read_command_line():
     """
     Read arguments from commandline
@@ -360,30 +381,23 @@ def read_command_line():
     # Add common arguments
     add_common_arguments(parser)
 
-    # Set region of interest:
-    parser.add_argument("-r", "--region-of-interest", type=str, default="manual",
-                        choices=["manual", "commandline", "landmarking"],
-                        help="The method for defining the region to be changed. There are" +
-                             " three options: 'manual', 'commandline', 'landmarking'. In" +
-                             " 'manual' the user will be provided with a visualization of the" +
-                             " input surface, and asked to provide an end and start point of the" +
-                             " region of interest. Note that not all algorithms are robust over" +
-                             " bifurcations. If 'commandline' is provided, then '--region-points'" +
-                             " is expected to be provided. Finally, if 'landmarking' is" +
-                             " given, it will look for the output from running automated_landmarking.py.")
-
-    parser.add_argument("--region-points", nargs="+", type=float, default=None, metavar="points",
-                        help="If -r or --region-of-interest is 'commandline' then this" +
-                             " argument have to be given. The method expects two points" +
-                             " which defines the start and end of the region of interest. " +
-                             " Example providing the points (1, 5, -1) and (2, -4, 3):" +
-                             " --region-points 1 5 -1 2 -4 3")
+    parser.add_argument('-bl', "--branch-loc", nargs="+", type=float, default=None, metavar="branch_location",
+                        help="If this parameter is provided, the branch to be manipulated will be moved to the point "
+                             "on the surface closest to this point. Example providing the point (1, 5, -1):" +
+                             " --region-points 1 5 -1")
 
     # Arguments for rotation
     parser.add_argument('-a', '--angle', type=float, default=0,
                         help="The manipulated branch is rotated an angle 'a' around the new" +
                              " surface normal vector. 'a' is assumed to be in degrees," +
                              " and not radians. Default is no rotation.", metavar="surface_normal_axis_angle")
+    # Argument for selecting branch
+    parser.add_argument('-bn', '--branch-number', type=int, default=0,
+                        help="The number corresponding the branch to manipulate. " +
+                             "The branches are ordered from 1 to N, " +
+                             "from upstream to downstream, relative to the inlet. " +
+                             "If not selected, the user must manually select the branch "
+                             "to manipulate. ", metavar="branch_number")
 
     # Parse paths to get default values
     args = parser.parse_args()
@@ -396,12 +410,11 @@ def read_command_line():
         if len(args.no_smooth_point) % 3 != 0:
             raise ValueError("ERROR: Please provide the no smooth point(s) as a multiple of 3")
 
-    return dict(input_filepath=args.ifile, smooth=args.smooth,
-                smooth_factor=args.smooth_factor,
+    return dict(input_filepath=args.ifile, smooth=args.smooth, smooth_factor=args.smooth_factor,
                 output_filepath=args.ofile, poly_ball_size=args.poly_ball_size,
                 no_smooth=args.no_smooth, no_smooth_point=args.no_smooth_point,
-                region_of_interest=args.region_of_interest, region_points=args.region_points,
-                resampling_step=args.resampling_step, angle=angle_to_radians)
+                resampling_step=args.resampling_step, angle=angle_to_radians,
+                branch_to_manipulate_number=args.branch_number, branch_location=args.branch_location)
 
 
 def main_branch():
