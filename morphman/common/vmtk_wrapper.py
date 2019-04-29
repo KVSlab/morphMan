a90114072dc8dc4a954590964f9f87e3d80dff3a
hkjeldsberg <henrik.kjeldsberg@live.no>
2019-04-29 02:30:41 +0200

    Added Copyright text in common files

--- morphman/common/vmtk_wrapper.py
+++ morphman/common/vmtk_wrapper.py
@@ -1,3 +1,10 @@
+##   Copyright (c) Aslak W. Bergersen, Henrik A. Kjeldsberg. All rights reserved.
+##   See LICENSE file for details.
+
+##      This software is distributed WITHOUT ANY WARRANTY; without even
+##      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+##      PURPOSE.  See the above copyright notices for more information.
+
 from vmtk import vtkvmtk, vmtkscripts
 
 from os import path
