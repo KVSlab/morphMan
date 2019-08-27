e4a6991977e6ce4b19236bf3e4224b0deb852588
hkjeldsberg <henrik.kjeldsberg@live.no>
2019-08-27 21:36:50 +0200

    Ignore conda update

--- continuous-integration/appveyor/install.ps1
+++ continuous-integration/appveyor/install.ps1
@@ -105,7 +105,7 @@ function UpdateCondaChannel ($python_home, $channels) {
 
 function main () {
     InstallMiniconda $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
-    UpdateConda $env:PYTHON
+    # UpdateConda $env:PYTHON
     InstallCondaPackages $env:PYTHON "conda-build==3.10.5 jinja2 anaconda-client"
     UpdateCondaChannel $env:PYTHON "vmtk"
 }
