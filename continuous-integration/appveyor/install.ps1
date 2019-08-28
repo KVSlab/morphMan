c4c3ffaea18a276c5ff4f1a69661d83ee8f12809
henrik.kjeldsberg <henrik.kjeldsberg@vipps.no>
2019-08-28 08:50:40 +0200

    Only install miniconda

--- continuous-integration/appveyor/install.ps1
+++ continuous-integration/appveyor/install.ps1
@@ -105,9 +105,9 @@ function UpdateCondaChannel ($python_home, $channels) {
 
 function main () {
     InstallMiniconda $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
-    UpdateConda $env:PYTHON
-    InstallCondaPackages $env:PYTHON "conda-build==3.10.5 jinja2 anaconda-client"
-    UpdateCondaChannel $env:PYTHON "vmtk"
+#    UpdateConda $env:PYTHON
+#    InstallCondaPackages $env:PYTHON "conda-build==3.10.5 jinja2 anaconda-client"
+#    UpdateCondaChannel $env:PYTHON "vmtk"
 }
 
 main
