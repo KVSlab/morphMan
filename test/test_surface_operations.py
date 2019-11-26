7a0ae932a35957b8b22a15e31b4fa89b3349e4a4
henrik.kjeldsberg <henrik.kjeldsberg@vipps.no>
2019-11-26 14:15:50 +0100

    Remove duplicate method for getting centers

--- test/test_surface_operations.py
+++ test/test_surface_operations.py
@@ -6,6 +6,8 @@
 ##      PURPOSE.  See the above copyright notices for more information.
 
 # noinspection PyUnresolvedReferences
+from IPython import embed
+
 from .fixtures import surface_paths
 from morphman.common.surface_operations import *
 
@@ -25,3 +27,17 @@ def test_relevant_outlets(surface_paths):
 
     assert outlets[0] == [0.1, 0.1, 0.1]
     assert outlets[1] == [1, 1, 1]
+
+
+def test_compute_centers(surface_paths):
+    input_filepath = surface_paths[0]
+    base_path = get_path_names(input_filepath)
+    surface = read_polydata(input_filepath)
+    centerlines = read_polydata(base_path + "_centerline.vtp")
+
+    inlet, outlets = compute_centers(surface, base_path)
+
+    assert len(inlet) // 3 == 1
+    assert len(outlets) // 3 == centerlines.GetNumberOfLines()
+
+
