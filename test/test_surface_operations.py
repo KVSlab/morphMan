f01cae135a8a7956912e2621bee3212dc9b0d79c
henrik.kjeldsberg <henrik.kjeldsberg@vipps.no>
2019-11-26 14:32:55 +0100

    Added more surface tests

--- test/test_surface_operations.py
+++ test/test_surface_operations.py
@@ -5,7 +5,6 @@
 ##      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 ##      PURPOSE.  See the above copyright notices for more information.
 
-
 from .fixtures import surface_paths
 from morphman.common.surface_operations import *
 
@@ -39,10 +38,36 @@ def test_compute_centers(surface_paths):
     assert len(outlets) // 3 == centerlines.GetNumberOfLines()
 
 
-
-def test_clip_capped_surface(surface_paths):
+def test_compute_area_ratio(surface_paths):
     input_filepath = surface_paths[0]
-    base_path = get_path_names(input_filepath)
     surface = read_polydata(input_filepath)
-    centerlines = read_polydata(base_path + "_centerline.vtp")
 
+    area_ratio, center = compute_circleness(surface)
+
+    assert type(area_ratio) == np.float64
+    assert len(center) // 3 == 1
+
+
+def test_capped_surface(surface_paths):
+    input_filepath = surface_paths[0]
+    surface = read_polydata(input_filepath)
+    capped = vmtk_cap_polydata(surface)
+
+    is_capped, number_outlets = is_surface_capped(surface)
+    assert not is_capped
+
+    is_capped, _ = is_surface_capped(capped)
+    assert is_capped
+
+    assert number_outlets == 7
+
+
+def test_uncapp_surface(surface_paths):
+    input_filepath = surface_paths[0]
+    surface = read_polydata(input_filepath)
+    capped = vmtk_cap_polydata(surface)
+
+    uncapped = get_uncapped_surface(capped)
+
+    is_capped, _ = is_surface_capped(uncapped)
+    assert not is_capped
