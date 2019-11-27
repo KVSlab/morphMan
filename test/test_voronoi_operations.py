eb794796dd71b6c859c55923862d1aed863535f5
henrik.kjeldsberg <henrik.kjeldsberg@vipps.no>
2019-11-27 10:55:12 +0100

    Added voronoi ops tests

--- test/test_voronoi_operations.py
+++ test/test_voronoi_operations.py
@@ -9,113 +9,53 @@ from .fixtures import surface_paths
 from morphman.common.surface_operations import *
 
 
-def test_relevant_outlets(surface_paths):
+def test_remove_distant_points(surface_paths):
+    input_filepath = surface_paths[0]
+    base_path = get_path_names(input_filepath)
+    centerlines = read_polydata(base_path + "_centerline.vtp")
+    voronoi = read_polydata(base_path + "_voronoi.vtp")
+
+    voronoi_removed = remove_distant_voronoi_points(voronoi, centerlines)
+
+    assert voronoi_removed.GetNumberOfPoints() < voronoi.GetNumberOfPoints()
+
+
+def test_smooth_voronoi_diagram(surface_paths):
+    input_filepath = surface_paths[0]
+    base_path = get_path_names(input_filepath)
+    centerlines = read_polydata(base_path + "_centerline.vtp")
+    voronoi = read_polydata(base_path + "_voronoi.vtp")
+
+    voronoi_smoothed = smooth_voronoi_diagram(voronoi, centerlines, 0.95)
+
+    assert voronoi_smoothed.GetNumberOfPoints() == voronoi.GetNumberOfPoints()
+
+
+def test_create_new_surface(surface_paths):
     input_filepath = surface_paths[0]
     base_path = get_path_names(input_filepath)
     surface = read_polydata(input_filepath)
+    voronoi = read_polydata(base_path + "_voronoi.vtp")
 
-    # Provide fake outlets
-    params = {'relevant_outlet_1': [0.1, 0.1, 0.1], 'relevant_outlet_2': [1, 1, 1]}
-    write_parameters(params, base_path)
+    new_surface = create_new_surface(voronoi, poly_ball_size=[75, 75, 75])
 
-    outlets = get_relevant_outlets(surface, base_path)
-
-    assert len(outlets) == 2
-
-    assert outlets[0] == [0.1, 0.1, 0.1]
-    assert outlets[1] == [1, 1, 1]
+    # Sanity check - Compare high resolution surface with low poly version
+    assert new_surface.GetNumberOfCells() > 0
+    assert new_surface.GetNumberOfCells() < surface.GetNumberOfCells()
 
 
-def test_compute_centers(surface_paths):
+def test_split_voronoi_diagram(surface_paths):
     input_filepath = surface_paths[0]
     base_path = get_path_names(input_filepath)
-    surface = read_polydata(input_filepath)
+    voronoi = read_polydata(base_path + "_voronoi.vtp")
+
     centerlines = read_polydata(base_path + "_centerline.vtp")
 
-    inlet, outlets = compute_centers(surface, base_path)
+    centerline1 = extract_single_line(centerlines, 0)
+    centerline2 = extract_single_line(centerlines, 1)
 
-    assert len(inlet) // 3 == 1
-    assert len(outlets) // 3 == centerlines.GetNumberOfLines()
+    split_voronoi = get_split_voronoi_diagram(voronoi, [centerline1, centerline2])
 
+    assert len(split_voronoi) == 2
 
-def test_compute_area_ratio(surface_paths):
-    input_filepath = surface_paths[0]
-    surface = read_polydata(input_filepath)
-
-    area_ratio, center = compute_circleness(surface)
-
-    assert type(area_ratio) == np.float64
-    assert len(center) // 3 == 1
-
-
-def test_capped_surface(surface_paths):
-    input_filepath = surface_paths[0]
-    surface = read_polydata(input_filepath)
-    capped = vmtk_cap_polydata(surface)
-
-    is_capped, number_outlets = is_surface_capped(surface)
-    assert not is_capped
-
-    is_capped, _ = is_surface_capped(capped)
-    assert is_capped
-
-    assert number_outlets == 7
-
-
-def test_uncapp_surface(surface_paths):
-    input_filepath = surface_paths[0]
-    surface = read_polydata(input_filepath)
-    capped = vmtk_cap_polydata(surface)
-
-    uncapped = get_uncapped_surface(capped)
-
-    is_capped, _ = is_surface_capped(uncapped)
-    assert not is_capped
-
-
-def test_if_surface_is_merged(surface_paths):
-    input_filepath = surface_paths[0]
-    base_path = get_path_names(input_filepath)
-    surface = read_polydata(input_filepath)
-    capped = vmtk_cap_polydata(surface)
-    centerlines = read_polydata(base_path + "_centerline.vtp")
-
-    # Sanity check on input model
-    check_if_surface_is_merged(capped, centerlines, input_filepath)
-
-
-def test_compute_voronoi_diagram(surface_paths):
-    input_filepath = surface_paths[0]
-    base_path = get_path_names(input_filepath)
-    surface = read_polydata(input_filepath)
-    capped = vmtk_cap_polydata(surface)
-    centerlines = read_polydata(base_path + "_centerline.vtp")
-
-    voronoi = prepare_voronoi_diagram(capped, centerlines, base_path, False, 0, True, None, None, None, 0.1)
-
-    assert voronoi.GetNumberOfPoints() > 0
-
-
-def test_compute_centerlines(surface_paths):
-    input_filepath = surface_paths[0]
-    base_path = get_path_names(input_filepath)
-    surface = read_polydata(input_filepath)
-    centerlines = read_polydata(base_path + "_centerline.vtp")
-
-    inlet, outlets = compute_centers(surface, base_path)
-
-    assert len(inlet) // 3 == 1
-    assert len(outlets) // 3 == centerlines.GetNumberOfLines()
-
-
-def test_prepare_surface(surface_paths):
-    input_filepath = surface_paths[0]
-    base_path = get_path_names(input_filepath)
-
-    surface, capped_surface = prepare_surface(base_path, input_filepath)
-
-    is_closed = is_surface_capped(capped_surface)
-
-    assert is_closed
-
-    assert surface.GetNumberOfCells() > 0
+    assert split_voronoi[0].GetNumberOfCells() > split_voronoi[1].GetNumberOfCells()
