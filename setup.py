4fea17932ba36db6673992dd347c31c3ebdad1ee
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2022-01-05 14:37:34 +0100

    Specity encoding in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "rb") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 DEPENDENCIES = []  # 'scipy', 'numpy', 'vtk']
